5565bea25c02dad85732c9b585321264d67bda03
Chandler Dearden <chandlerjdearden@gmail.com>
2022-11-23 15:31:24 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use(express.static(`${__dirname}/public`))
+
+
+
+
+app.listen(4000, () => console.log("server is running on 4000"))
